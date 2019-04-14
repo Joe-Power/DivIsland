fc4dbfe76ae2257b57f52528155524b7520e6e0b
Catherine <31903385+CatWeng@users.noreply.github.com>
2019-04-14 01:15:36 -0400

    Break added
    Door now breaks apart

--- New Unity Project/Assets/Scripts/PressurePad.cs
+++ New Unity Project/Assets/Scripts/PressurePad.cs
@@ -5,19 +5,39 @@ using UnityEngine;
 public class PressurePad : MonoBehaviour
 {
   public GameObject door;
-  private static int doorint;
+  public static int doorint;
+  private bool broken;
+
+  //Integrating the Break script
+  public float cubeSize = 0.05f;
+  public int cubesInRow = 5;
+
+  float cubesPivotDistance;
+  Vector3 cubesPivot;
+
+  public float minExplosionForce = 1f;
+  public float maxExplosionForce = 20f;
+  public float explosionRadius = 4f;
+  public float explosionUpward = 0.4f;
+  public float removeDelay = 3f;
 
   void Start ()
   {
     door = GameObject.FindWithTag("Test");
-    doorint = 2;
+    doorint = 1;
+    broken = false;
+    // From the Break script
+    // gameObject.AddComponent<Rigidbody>();
+    cubesPivotDistance = cubeSize * cubesInRow / 2;
+    cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
   }
 
   void OnTriggerEnter(Collider other)
   {
     doorint = (doorint - 1);
-    if (doorint == 0) {
-      Destroy(door);
+    if (doorint == 0 && broken == false) {
+      Invoke("explode", 1);
+      broken = true;
       }
     Debug.Log (doorint);
   }
@@ -26,5 +46,35 @@ public class PressurePad : MonoBehaviour
   {
     doorint = (doorint + 1);
     Debug.Log (doorint);
+  }
+
+  // From the Break script
+  public void explode(){
+      door.SetActive(false);
+      for (int x = 0; x < cubesInRow; x++) {
+          for (int y = 0; y < cubesInRow; y++) {
+              for (int z = 0; z < cubesInRow; z++) {
+                  createPiece(x, y, z);
+              }
+          }
+      }
+      Vector3 explosionPos = door.transform.position;
+      Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+      foreach (Collider hit in colliders) {
+          Rigidbody rigbod = hit.GetComponent<Rigidbody>();
+          if(rigbod != null) {
+              rigbod.AddExplosionForce(Random.Range(minExplosionForce, maxExplosionForce), door.transform.position, explosionRadius, explosionUpward);
+          }
+      }
+  }
+
+  void createPiece(int x, int y, int z){
+      GameObject piece;
+      piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+      piece.transform.position = door.transform.position + new Vector3(cubeSize * x, (cubeSize * y + 6), cubeSize * z) - cubesPivot;
+      piece.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+      piece.AddComponent<Rigidbody>();
+      piece.GetComponent<Rigidbody>().mass = cubeSize;
+      Destroy(piece, removeDelay);
   }
 }
