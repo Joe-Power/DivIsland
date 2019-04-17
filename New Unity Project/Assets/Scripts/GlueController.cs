b135858f9f5e5d98f3f28be3285ddc3f84400ae0
jenpowroznyk <35268767+jenpowroznyk@users.noreply.github.com>
2019-04-17 15:31:21 -0400

    Add files via upload

---
+++ New Unity Project/Assets/Scripts/GlueController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlueController : MonoBehaviour
+{
+
+    public GameObject glue;
+    public GameObject glueData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ActivePlayer")
+        { 
+            glueData.GetComponent<GlueCounter>().AddGlue();
+            Destroy(glue);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+     
+
+    }
+}
