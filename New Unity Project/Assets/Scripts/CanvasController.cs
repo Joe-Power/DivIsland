a437b28bb19448a1c4be9c848679897033cdf547
jenpowroznyk <35268767+jenpowroznyk@users.noreply.github.com>
2019-04-18 01:06:43 -0400

    Added UI elements / game explaination

---
+++ New Unity Project/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public GameObject homeUI;
+    public GameObject inGameUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void ShowInGameUI()
+    {
+        homeUI.SetActive(false);
+        inGameUI.SetActive(true);
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("return"))
+        {
+            ShowInGameUI();
+
+        }
+    }
+}
